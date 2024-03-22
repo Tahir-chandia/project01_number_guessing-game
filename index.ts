f74a356e0e7511ad9dd874b0efcf0f730da8ffd3
Tahir Chandia <161021042+Tahir-chandia@users.noreply.github.com>
2024-03-23 03:29:03 +0500

    Created guessing game.

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+
+console.log("Welcome to Number Guessing Game");
+
+const randomNumber:number = Math.floor((Math.random()* 6 + 1));
+
+const answere = await inquirer.prompt([{
+    name: "userNumber",
+    type:"number",
+    message:"Enter your number between 1-6:",
+}]);
+
+if(randomNumber === answere.userNumber){
+
+    console.log(`Congratulations! "You Won" `);
+}
+else{
+    console.log("You loose");
+};
+console.log("Game over");
